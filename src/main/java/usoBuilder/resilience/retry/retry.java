fb8d45c93a9bc6cf3ee04b0ac8db5357ccdfbcaf
Miguel Angel <vivanlasfallas@hotmail.com>
2021-10-18 19:52:29 +0200

    prueba retry

--- src/main/java/usoBuilder/resilience/retry/retry.java
+++ src/main/java/usoBuilder/resilience/retry/retry.java
@@ -2,6 +2,46 @@ package usoBuilder.resilience.retry;
 
 public class retry {
 	
+	private final int MAX_INTENTOS= 3;
+	
+	private int intentos= 0;
+	private boolean conexion;
+	
+	
+	
+	
+	public retry() {
+		
+		++this.intentos;
+		
+		while(this.intentos != this.MAX_INTENTOS) {
+	
+			if(!this.conexion) { return;}
+			
+			else if (this.conexion) {
+				
+				System.out.println("Conectado con éxito");
+			}
+			
+			
+		}
+			
+	}
+	
+	
+	
+	public int getIntentos() {
+		return intentos;
+	}
+	
+	
+	public void setIntentos(int intentos) {
+		this.intentos = intentos;
+	}
+	
+	
+	
+	
 	
 	
 	
